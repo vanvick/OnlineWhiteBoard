8348e110643afdabb7dc1d0a14fec1e9498fd1a4
root <root@ubuntu.ubuntu-domain>
2013-04-13 13:20:32 +0800

    MemoryCache

---
+++ server/DataProvider/MemoryCache.cc
@@ -0,0 +1,166 @@
+/*************************************************************************
+     ** File Name :  ServerDelegate.h
+    ** Author :  fl570
+    ** Mail :  cqfl570@gmail.com
+    ** Created Time :  Wed Apr 10 16 : 46 : 51 2013
+    **Copyright [2013]  <Copyright fl570>   [legal/copyright]
+ ************************************************************************/
+
+#include "./MemoryCache.h"
+
+using namespace Kingslanding::OnlineWhiteBoard::Server::DataProvider;
+
+MemoryCache::MemoryCache(int capacity) {
+  state_ = false;
+  capacity_ = capacity;
+  front_[0]=0;
+  front_[1]=0;
+  rear_[0]=0;
+  rear_[1]=0;
+  size_[0]=0;
+  size_[1]=0;
+  front_id_[0]=0;
+  front_id_[1]=0;
+  rear_id_[0]=0;
+  rear_id_[1]=0;
+
+  if (capacity <= 0) {
+    LOG(ERROR) << "ERROR: capacity <=0 !";
+  } else {
+    try {
+      operation_[0] = new const Operation*[capacity_];
+      operation_[1] = new const Operation*[capacity_];
+    }
+    catch(std::bad_alloc&) {
+      LOG(ERROR) << "ERROR: cann't create operation_ set !";
+    }
+  }
+
+}
+
+MemoryCache::~MemoryCache() {
+  if (operation_[0]) {
+    for (int i = 0; i < size_[0]; i++) {
+      index_ = front_[0];
+      delete operation_[0][index_];
+      front_[0]++;
+    }
+    delete []operation_[0];
+  }
+  if (operation_[1]) {
+    for (int i = 0; i < size_[1]; i++) {
+      index_ = front_[1];
+      delete operation_[1][index_];
+      front_[1]++;
+    }
+    delete []operation_[1];
+  }
+}
+
+int MemoryCache::IsEmpty() const {
+    return 0 == (state_ ? size_[1] : size_[0]);
+}
+
+int MemoryCache::GetCount() const {
+    return state_ ? size_[1] : size_[0];
+}
+
+int MemoryCache::IsFull() const {
+    return capacity_ == (state_ ? size_[1] : size_[0]);
+}
+
+bool MemoryCache::SetState() {
+    if (state_) {
+      for (int i = 0; i < size_[0]; i++) {
+        index_ = front_[0];
+        delete operation_[0][index_];
+        front_[0]++;
+      }
+      rear_[0] = 0;
+      front_[0] = 0;
+      size_[0] = 0;
+      front_id_[0] = 0;
+      rear_id_[0] = 0;
+    } else {
+      for (int i = 0; i < size_[1]; i++) {
+        index_ = front_[1];
+        delete operation_[1][index_];
+        front_[1]++;
+      }
+      rear_[1] = 0;
+      front_[1] = 0;
+      size_[1] = 0;
+      front_id_[1] = 0;
+      rear_id_[1] = 0;
+    }
+    state_ = state_ ? false : true;
+}
+
+
+bool MemoryCache::AddOperation(const Operation* oper) {
+  if (state_) {
+    AddOperationToSet(1, oper);
+  } else {
+    AddOperationToSet(0, oper);
+  }
+
+  return true;
+}
+
+bool MemoryCache::AddOperationToSet(int set, const Operation* oper) {
+
+    if (IsEmpty()) {
+      rear_[set] = -1;
+      front_id_[set] = oper->serial_number();
+      rear_id_[set] = oper->serial_number();
+    }
+    if (IsFull()) {
+      index_ = front_[set];
+      rear_[set] = index_;
+      delete operation_[set][index_];
+      operation_[set][index_] = oper;
+
+      front_[set]++;
+      front_[set] %= capacity_;
+      index_ = front_[set];
+      front_id_[set] = operation_[set][index_] -> serial_number();
+    } else {
+      size_[set]++;
+      rear_[set]++;
+      rear_[set] %= capacity_;
+      index_ = rear_[set];
+      operation_[set][index_] = oper;
+    }
+
+    rear_id_[set] = oper->serial_number();
+    return true;
+}
+
+Operations MemoryCache::GetOperationAfter(int operation_id) {
+  return state_ ? GetOperationFromSet(1, operation_id) : GetOperationFromSet(0, operation_id);
+}
+
+Operations MemoryCache::GetOperationFromStoreAfter(int operation_id) {
+  return state_ ? GetOperationFromSet(0, operation_id) : GetOperationFromSet(1, operation_id);
+}
+
+
+Operations MemoryCache::GetOperationFromSet(int set, int operation_id) {
+  Operations opers;
+  Operation *oper;
+  if ((operation_id+1) < front_id_[set]) {
+    opers.set_operation_avaliable(false);
+  } else {
+    opers.set_operation_avaliable(true);
+    index_ = operation_id+1;
+    for (int i = 0; i < rear_id_[set]-operation_id; i++) {
+      oper = opers.add_operations();
+      *oper = *operation_[set][index_];
+      index_++;
+    }
+  }
+  return opers;
+}
+
+
+
---
+++ server/DataProvider/MemoryCacheTest.cc
@@ -0,0 +1,222 @@
+#include "./MemoryCache.h"
+
+namespace Kingslanding {
+namespace OnlineWhiteBoard {
+namespace Server {
+namespace DataProvider {
+
+class MemoryCacheTest : public ::testing::Test {
+ protected:
+  MemoryCache *q0_ ;
+  Operation **oper;
+  static void SetUpTestCase(){
+    GOOGLE_PROTOBUF_VERIFY_VERSION;
+    google::InitGoogleLogging("LOG");
+    FLAGS_log_dir = "../LOG";
+  }
+  static void TearDownTestCase() {
+    google::protobuf::ShutdownProtobufLibrary();
+    google::ShutdownGoogleLogging();  
+  }
+  virtual void SetUp() {
+    q0_ = new MemoryCache(5);
+    oper = new Operation*[6];
+    Operation_OperationData *data;
+    Operation_OperationData_Point *point;
+    for (int i=0; i<6; i++) {
+      oper[i] = new Operation;
+      oper[i]->set_serial_number(i);
+      data = oper[i]->mutable_data();
+      data -> set_data_type(Operation_OperationData_OperationDataType_LINE);
+      data -> set_thinkness(30+i);
+      data -> set_color(30+i);
+      data -> set_a(30+i);
+      data -> set_b(40+i);
+      data -> set_fill(true);
+      point = data -> mutable_start_point();
+      point -> set_x(30+i);
+      point -> set_y(40+i);
+    }
+    for (int i=0; i<4; i++) {
+      q0_->AddOperation(oper[i]);
+    }
+  }
+  virtual void TearDown() {
+    delete oper;
+  }
+
+  
+};
+
+TEST_F(MemoryCacheTest, Initial) {
+    EXPECT_FALSE(q0_->state_);
+    EXPECT_EQ(5, q0_->capacity_);
+    EXPECT_EQ(0, q0_->front_[0]);
+    EXPECT_EQ(0, q0_->front_[1]);
+    EXPECT_EQ(3, q0_->rear_[0]);
+    EXPECT_EQ(0, q0_->rear_[1]);
+    EXPECT_EQ(0, q0_->front_id_[0]);
+    EXPECT_EQ(3, q0_->rear_id_[0]);
+    EXPECT_EQ(0, q0_->front_id_[1]);
+    EXPECT_EQ(0, q0_->rear_id_[1]);
+    EXPECT_EQ(0, q0_->size_[1]);
+    EXPECT_EQ(4, q0_->size_[0]);
+}
+
+TEST_F(MemoryCacheTest, AddOperation) {
+
+    q0_->AddOperation(oper[4]);
+    EXPECT_FALSE(q0_->state_);
+    EXPECT_EQ(5, q0_->capacity_);
+    EXPECT_EQ(0, q0_->front_[0]);
+    EXPECT_EQ(0, q0_->front_[1]);
+    EXPECT_EQ(4, q0_->rear_[0]);
+    EXPECT_EQ(0, q0_->rear_[1]);
+    EXPECT_EQ(0, q0_->front_id_[0]);
+    EXPECT_EQ(4, q0_->rear_id_[0]);
+    EXPECT_EQ(0, q0_->front_id_[1]);
+    EXPECT_EQ(0, q0_->rear_id_[1]);
+    EXPECT_EQ(0, q0_->size_[1]);
+    EXPECT_EQ(5, q0_->size_[0]);
+
+    q0_->AddOperation(oper[5]);
+    EXPECT_FALSE(q0_->state_);
+    EXPECT_EQ(5, q0_->capacity_);
+    EXPECT_EQ(1, q0_->front_[0]);
+    EXPECT_EQ(0, q0_->front_[1]);
+    EXPECT_EQ(0, q0_->rear_[0]);
+    EXPECT_EQ(0, q0_->rear_[1]);
+    EXPECT_EQ(1, q0_->front_id_[0]);
+    EXPECT_EQ(5, q0_->rear_id_[0]);
+    EXPECT_EQ(0, q0_->front_id_[1]);
+    EXPECT_EQ(0, q0_->rear_id_[1]);
+    EXPECT_EQ(0, q0_->size_[1]);
+    EXPECT_EQ(5, q0_->size_[0]);
+}
+
+TEST_F(MemoryCacheTest, GetOperationAfter) {
+    Operations opers = q0_->GetOperationAfter(-2);
+    EXPECT_FALSE(opers.operation_avaliable());
+    
+    
+    opers = q0_->GetOperationAfter(-1);
+    EXPECT_EQ(4, opers.operations_size());
+    EXPECT_EQ(true, opers.operation_avaliable());
+
+    const Operation* oper;
+    const Operation_OperationData *data;
+    const Operation_OperationData_Point *point;
+
+    for(int i=0; i < opers.operations_size(); i++) {
+      oper = &opers.operations(i);
+      EXPECT_EQ(i, oper -> serial_number());
+      data = &oper -> data();
+      EXPECT_EQ(Operation_OperationData_OperationDataType_LINE, data -> data_type());
+      point = &data -> start_point();
+      EXPECT_EQ((i+30), point -> x());
+      EXPECT_EQ((i+40), point -> y());
+    }
+
+    opers = q0_->GetOperationAfter(3);
+    EXPECT_EQ(0, opers.operations_size());
+    EXPECT_TRUE(opers.operation_avaliable());
+
+    opers = q0_->GetOperationAfter(4);
+    EXPECT_EQ(0, opers.operations_size());
+    EXPECT_TRUE(opers.operation_avaliable());
+    
+}
+
+TEST_F(MemoryCacheTest, GetOperationFromStoreAfter) {
+    q0_->SetState();
+
+    Operations opers = q0_->GetOperationFromStoreAfter(-2);
+    EXPECT_FALSE(opers.operation_avaliable());
+    
+    opers = q0_->GetOperationFromStoreAfter(-1);
+    EXPECT_EQ(4, opers.operations_size());
+    EXPECT_TRUE(opers.operation_avaliable());
+
+    const Operation* oper;
+    const Operation_OperationData *data;
+    const Operation_OperationData_Point *point;
+
+    for(int i=0; i < opers.operations_size(); i++) {
+      oper = &opers.operations(i);
+      EXPECT_EQ(i, oper -> serial_number());
+      data = &oper -> data();
+      EXPECT_EQ(Operation_OperationData_OperationDataType_LINE, data -> data_type());
+      point = &data -> start_point();
+      EXPECT_EQ((i+30), point -> x());
+      EXPECT_EQ((i+40), point -> y());
+    }
+
+    opers = q0_->GetOperationFromStoreAfter(3);
+    EXPECT_EQ(0, opers.operations_size());
+    EXPECT_TRUE(opers.operation_avaliable());
+
+    opers = q0_->GetOperationFromStoreAfter(4);
+    EXPECT_EQ(0, opers.operations_size());
+    EXPECT_TRUE(opers.operation_avaliable());
+
+}
+
+TEST_F(MemoryCacheTest, SetState) {
+    q0_->SetState();
+    EXPECT_TRUE(q0_->state_);
+    EXPECT_EQ(5, q0_->capacity_);
+    EXPECT_EQ(0, q0_->front_[0]);
+    EXPECT_EQ(0, q0_->front_[1]);
+    EXPECT_EQ(3, q0_->rear_[0]);
+    EXPECT_EQ(0, q0_->rear_[1]);
+    EXPECT_EQ(0, q0_->front_id_[0]);
+    EXPECT_EQ(3, q0_->rear_id_[0]);
+    EXPECT_EQ(0, q0_->front_id_[1]);
+    EXPECT_EQ(0, q0_->rear_id_[1]);
+    EXPECT_EQ(0, q0_->size_[1]);
+    EXPECT_EQ(4, q0_->size_[0]);
+
+    for (int i=4; i<6; i++) {
+      q0_->AddOperation(oper[i]);
+    }
+    
+    EXPECT_TRUE(q0_->state_);
+    EXPECT_EQ(5, q0_->capacity_);
+    EXPECT_EQ(0, q0_->front_[0]);
+    EXPECT_EQ(0, q0_->front_[1]);
+    EXPECT_EQ(3, q0_->rear_[0]);
+    EXPECT_EQ(1, q0_->rear_[1]);
+    EXPECT_EQ(0, q0_->front_id_[0]);
+    EXPECT_EQ(3, q0_->rear_id_[0]);
+    EXPECT_EQ(4, q0_->front_id_[1]);
+    EXPECT_EQ(5, q0_->rear_id_[1]);
+    EXPECT_EQ(2, q0_->size_[1]);
+    EXPECT_EQ(4, q0_->size_[0]);
+
+    q0_->SetState();
+    EXPECT_FALSE(q0_->state_);
+    EXPECT_EQ(5, q0_->capacity_);
+    EXPECT_EQ(0, q0_->front_[0]);
+    EXPECT_EQ(0, q0_->front_[1]);
+    EXPECT_EQ(0, q0_->rear_[0]);
+    EXPECT_EQ(1, q0_->rear_[1]);
+    EXPECT_EQ(0, q0_->front_id_[0]);
+    EXPECT_EQ(0, q0_->rear_id_[0]);
+    EXPECT_EQ(4, q0_->front_id_[1]);
+    EXPECT_EQ(5, q0_->rear_id_[1]);
+    EXPECT_EQ(2, q0_->size_[1]);
+    EXPECT_EQ(0, q0_->size_[0]);
+    
+     
+}
+
+}  // DataProvider
+}  // Server
+}  // OnlineWhiteBoard
+}  // Kingslanding
+
+int main(int argc, char **argv){
+  std::cout << "Running main() from gtest_main.cc\n";
+  testing::InitGoogleTest(&argc, argv);  
+  return RUN_ALL_TESTS();
+}
